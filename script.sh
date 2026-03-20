f8bf9712ece1c72a2f1f9fedb4c65c4d8716df92
Luann Pereira Mendes <luannpmendes@gmail.com>
2026-03-19 23:52:47 -0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,6 @@
+sudo xboxdrv --evdev /dev/input/by-id/usb-8BitDo_8BitDo_Ultimate_2C_Wireless_Controller_D08018A781-event-joystick \
+--evdev-absmap ABS_X=x1,ABS_Y=y1,ABS_RX=x2,ABS_RY=y2,ABS_Z=lt,ABS_RZ=rt,ABS_HAT0X=dpad_x,ABS_HAT0Y=dpad_y \
+--evdev-keymap BTN_SOUTH=a,BTN_EAST=b,BTN_NORTH=x,BTN_WEST=y,BTN_TL=lb,BTN_TR=rb,BTN_THUMBL=tl,BTN_THUMBR=tr,BTN_SELECT=back,BTN_START=start,BTN_MODE=guide \
+--axismap -y1=y1,-y2=y2 \
+--mimic-xpad --silent</code></pre>
+  
